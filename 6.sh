a42df2293103fa357fb954ee477d3705b683841a
rajdeep-20 <112775823+rajdeep-20@users.noreply.github.com>
2023-11-24 19:50:01 +0530

    Delete 6.sh

--- 6.sh
+++
@@ -1,6 +0,0 @@
-i=1                          # Initialize a variable 'i' with the value 1.
-while [ $i -le 10 ]           # Start a while loop that continues while 'i' is less than or equal to 10.
-do                           # Begin the loop.
-  echo "$i"                  # Print the value of 'i'.
-  i=$(expr $i + 1)           # Update the value of 'i' by incrementing it.
-done                         # End the loop.
